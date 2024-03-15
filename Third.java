cab470e9cac356a80739fd57bf78e1dd8620abe4
Mengleang <161790324+Mengleang30@users.noreply.github.com>
2024-03-15 08:17:43 +0700

    Create Third.java

---
+++ Third.java
@@ -0,0 +1,10 @@
+public class Third {
+    public static void main(String[] args) {
+        int var = 10;
+        double var1 = 35;
+        double sum = var + var1;
+        System.out.println(sum);
+
+    }
+    
+}
