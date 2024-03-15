d55f5cc7ed73154fe11924f1e0496703756a79fd
Mengleang <novmengleang14@gmail.com>
2024-03-15 08:23:29 +0700

    Update second.java

--- second.java
+++ second.java
@@ -1,8 +1,15 @@
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
 
 public class second {
 
 }
 class test extends JFrame{
+    @Override
+    public void printAll(Graphics g) {
+        // TODO Auto-generated method stub
+        super.printAll(g);
+    }
     
 }
